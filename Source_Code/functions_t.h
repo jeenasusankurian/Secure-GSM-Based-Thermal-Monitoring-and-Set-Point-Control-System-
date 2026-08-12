1daaa12a66d6bf62d4e9c934817a6f08727a6800
jeenasusankurian <jeenasusank19@gmail.com>
2026-08-12 10:36:43 +0530

    Update and rename functions_t.h to Source_Code/functions_t.h

---
+++ Source_Code/functions_t.h
@@ -0,0 +1,14 @@
+//functions_t.h
+void Enable_EINT0(void);
+void menu_function(void);
+
+#define BUZZER     18   // P0.18
+#define GREEN_LED  19   // P0.19
+#define RED_LED    20   // P0.20
+
+
+#define ADDR_PASS  0x0400
+#define ADDR_MOB   0x0500
+#define ADDR_TEMP  0x0600
+#define ADDR_HUM   0x0700
+
